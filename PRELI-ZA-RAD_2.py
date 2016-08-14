6999b27095dd132acca8ca2edc72d33ac2ba7562
Boris Majić <boris.majic@gmail.com>
2016-08-14 20:10:53 +0200

    Commented version
    As the title says, maybe with some minor changes

--- PRELI-ZA-RAD_2.py
+++ PRELI-ZA-RAD_2.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 from numpy.linalg import eig, inv
 import glob
 
+#Ellipse fitting function, returns ellipse parameters
+#Only method for calculating semiaxis is used, the rest is available in
+#the Utilities repository
 def ellip_fit(x,y):
     x = x[:,np.newaxis]
     y = y[:,np.newaxis]
@@ -21,7 +24,8 @@ def ellip_fit(x,y):
     n = np.argmax(np.abs(E))
     a = V[:,n]
     return a
-
+    
+#Determining semiaxis from ellipse parameters
 def ellip_ose( a ):
     b,c,d,f,g,a = a[1]/2, a[2], a[3]/2, a[4]/2, a[5], a[0]
     up = 2*(a*f*f+c*d*d+g*b*b-2*b*d*f-a*c*g)
@@ -34,58 +38,58 @@ def ellip_ose( a ):
 outputVals = np.empty([0,1], dtype=np.float)
 imageFiles = np.empty([0,1], dtype=np.str)
 
+#TODO: Enable setting input directory
+
 for file in glob.glob("DSC*.jpg"):
     imageFiles = np.append(imageFiles, file)
     currImage = Image.open(file)
     print(file)
-#slika = Image.open("DSC07592.JPG")
+
     arej = np.asarray(currImage.convert('L'))
     format = currImage.size
-    #slika.show()
-    #print("Dimenzije slike su:", format)
     
     niz = np.asarray([])
     x = np.asarray([])
     yprvo = -1
     
+    #Passing through image lines and finding edges of the Moon
     for y in range (0, format[1]):
         intenzitet = arej[y]
-        prvi = -1
-        poslednji = -1
+        first = -1
+        last = -1
         for i in range(format[0]):
             if intenzitet[i] > 30 :
-                if prvi == -1 :
-                    prvi = i
+                if first == -1 :
+                    first = i
                 else:
-                    poslednji = i
+                    last = i
     
-        if prvi ==-1:
+        if first ==-1:
             pass
-        if poslednji == -1 :
+        if last == -1 :
             pass
-        rastojanje = poslednji - prvi
+        rastojanje = last - first
         if rastojanje > 30 and rastojanje < 2000:
             if yprvo == -1:
                 yprvo = y
-            rastojanje = poslednji - prvi
+            rastojanje = last - first
             #niz = np.append(niz,rastojanje)
             x = np.append(x, y)
             x = np.append(x, y)
-            niz = np.append(niz, prvi)
-            niz = np.append(niz, poslednji)        
+            niz = np.append(niz, first)
+            niz = np.append(niz, last)        
             #x.append(y-yprvo)
     
-    a = ellip_fit(x[1:100], niz[1:100])
+    #Fitting the elipse and depositing current image major semiaxis to an array
+    a = ellip_fit(x, niz)
     axes = ellip_ose(a)
-    m, v = axes                  #m = mala poluosa; v = velika poluosa
-    #print('ove su poluose', m, v)
-    #print('Velika polusa: ', max(m,v))
+    m, v = axes                 
     outputVals = np.append(outputVals, max(m,v))
     currImage.close()
-    #plt.scatter(x, niz)
-    #plt.show()
-print('Gotovo!')
-plt.plot(outputVals)
-plt.show()
+
+print('Finished!')
+#TODO: Export array of semiaxis to a file
+#plt.plot(outputVals)
+#plt.show()
 
 
